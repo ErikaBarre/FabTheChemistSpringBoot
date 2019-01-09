f9f627d14463ee5c321630824a6a986821525385
fabrice <fabrice@schopenhauer>
2019-01-09 23:28:45 +0100

    45

--- springbootjpa/src/main/resources/data.sql
+++ springbootjpa/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-create table fab_person
+/*create table fab_person
 (
 PE_ID                    integer not null,
 PE_NAME                           varchar(255) not null,
@@ -6,6 +6,7 @@ PE_LOCATION   varchar(255),
 PE_BIRTH_DATE               timestamp,
 primary key(PE_ID)
 );
+*/
 insert into fab_person (PE_ID  ,PE_NAME,PE_LOCATION,PE_BIRTH_DATE) values (10001, 'Muriel', 'Namur', sysdate());
 insert into fab_person (PE_ID  ,PE_NAME,PE_LOCATION,PE_BIRTH_DATE) values (10002, 'James', 'Compos', sysdate());
 insert into fab_person (PE_ID  ,PE_NAME,PE_LOCATION,PE_BIRTH_DATE) values (10003, 'Erika', 'Barre', sysdate());
