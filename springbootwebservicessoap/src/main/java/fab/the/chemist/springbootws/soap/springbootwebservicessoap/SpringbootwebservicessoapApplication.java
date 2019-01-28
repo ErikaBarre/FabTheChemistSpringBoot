505a2f33ab31e540f19cdb35609db56334035a38
fabrice <fabrice@schopenhauer>
2019-01-28 23:08:37 +0100

    11111

---
+++ springbootwebservicessoap/src/main/java/fab/the/chemist/springbootws/soap/springbootwebservicessoap/SpringbootwebservicessoapApplication.java
@@ -0,0 +1,14 @@
+package fab.the.chemist.springbootws.soap.springbootwebservicessoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebservicessoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwebservicessoapApplication.class, args);
+	}
+
+}
+
