f47e64d5b2d9b25b6c48e6436b2be26c4d1ae114
fabrice <fabrice@schopenhauer>
2019-02-17 09:38:06 +0100

    1.0

---
+++ springbootrestfullws/src/main/java/net/fab/the/chemist/springbootrestfullws/SpringbootrestfullwsApplication.java
@@ -0,0 +1,13 @@
+package net.fab.the.chemist.springbootrestfullws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrestfullwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrestfullwsApplication.class, args);
+	}
+
+}
---
+++ springbootrestfullws/src/test/java/net/fab/the/chemist/springbootrestfullws/SpringbootrestfullwsApplicationTests.java
@@ -0,0 +1,16 @@
+package net.fab.the.chemist.springbootrestfullws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootrestfullwsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
